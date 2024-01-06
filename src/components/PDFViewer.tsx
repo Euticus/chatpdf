3b2c2c07a390c5f8e6b3cc7e51bc73c45d13b8d4
Ryan Miller <ryanmiller@Ryans-MacBook-Pro.local>
2024-01-05 22:33:51 -0800

    changed PDFViewer

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -5,7 +5,7 @@ type Props = { pdf_url: string };
 const PDFViewer = ({ pdf_url }: Props) => {
   return (
     <iframe
-    src={`https://docs.google.com/gview?url=${encodeURIComponent(pdf_url)}&embedded=true`}
+    src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
     className="w-full h-full"
     ></iframe>
   );
